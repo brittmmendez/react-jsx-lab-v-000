76ebfaa69df3422d7353c2efb372fbb251b17772
Brittany Morris <brittmmendez@gmail.com>
2018-04-24 01:47:17 +0000

    automatically backed up by learn

---
+++ src/components/Webpage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Webpage extends React.Component {
+  render (){
+    return (
+      <div>
+        <h1> The world's coolest webpage </h1>
+          <FillerText />
+          <FillerText />
+      </div>
+    )
+  }
+}
+
+export default Webpage;
